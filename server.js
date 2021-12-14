10bad5e930acea91db3b068aaad678bf6722c6a6
yuipapa <yuipapa0206@gmail.com>
2021-12-14 16:54:37 +0900

    Add Material UI Design for Add/Delete

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const upload = multer({dest: './upload'});
 
 app.get('/api/customers',(req,res) => {
     connection.query(
-        "SELECT * FROM management.customer",
+        'SELECT * FROM management.customer where isDeleted = 0 ',
         (err, rows, fields) => {
             res.send(rows);
         }
@@ -39,7 +39,7 @@ app.get('/api/customers',(req,res) => {
 app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req,res) => {
-    let sql = 'insert into management.customer values (null, ?, ?, ?, ?, ?)';
+    let sql = 'insert into management.customer values (null, ?, ?, ?, ?, ?, now(), 0)';
     let image = '/image/' + req.file.filename;
     let name =req.body.name;
     let birthday =req.body.birthday;
@@ -51,6 +51,16 @@ app.post('/api/customers', upload.single('image'), (req,res) => {
                 res.send(rows);
             }
         )
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+    let sql = 'update management.customer set isDeleted = 1 where id = ?';
+    let params = [req.params.id];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
 });
 
 app.listen(port,() => console.log(`Listening on port ${port}`));
