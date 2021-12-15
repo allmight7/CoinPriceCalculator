83f96c3bd3cb70e8eb0f3bd4f6c6e4ee39b00a17
yuipapa <yuipapa0206@gmail.com>
2021-12-15 11:53:09 +0900

    Update UI/UX & Add Customer Search Module

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Noto Sans KR", serif'
+  }
+})
+
+ReactDOM.render(<MuiThemeProvider theme={theme}><React.StrictMode><App /></React.StrictMode></MuiThemeProvider>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
